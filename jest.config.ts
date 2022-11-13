300b5975eccf9975523b71891e7dad112d2f3bb0
Justin Kimbrell <jkimbrell@actengage.com>
2022-11-12 20:01:08 -0700

    New coverage report and badges.

--- jest.config.ts
+++ jest.config.ts
@@ -35,12 +35,13 @@ export default {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    "json",
+    "json-summary",
+    "text",
+    "lcov",
+    "clover"
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
